49295ef0548a0a685ebc034fc5408902580f06db
Sasha Ms <zontick99@gmail.com>
2021-03-12 08:13:17 +0200

    tk

---
+++ question/prog.py
@@ -0,0 +1,8 @@
+import requests, bs4
+
+res = requests.get('https://www.i.ua/')
+a = res.raise_for_status()
+print("1---", a)
+
+res_st = bs4.BeautifulSoup(res.text)
+print('2---', res_st)
---
+++ tk_1.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox as mb
+
+root = tk.Tk()
+root.title("Сортировка файлов по расширению")
+root.geometry('600x500+200+100')
+
+#получаем адрес исходной папки и сохраняем в ! src_adr!
+def my_src():
+    if len(ent_src.get()) == 0: 
+        mb.showerror('ERROR',
+            'Поле 1 не должно быть пустым')
+    else:
+        src_adr = ent_src.get() 
+        print(src_adr)
+
+#получаем адрес назначения и сохраняем в !dts_adr!
+def my_dts():
+    if len(ent_dst.get()) == 0: 
+        mb.showerror('ERROR',
+            'Поле 2 не должно быть пустым')
+    else:
+        dts_adr = ent_dst.get() 
+        print(dts_adr)
+
+#получаем расширение назначения и сохраняем в !expancion_adr!
+def expancion():
+    if len(expancions_touch.get()) == 0: 
+        mb.showerror('ERROR',
+            'Поле 3 не должно быть пустым')
+    else:
+        expancion_adr = expancions_touch.get() 
+        print(expancion_adr)
+
+#Вызываем функции и охраняем получаный результат 
+def touch_adr():
+    try:
+        my_src()
+        my_dts()
+        expancion()
+    except Exception as ex:
+        mb.showerror(ex)
+
+
+#Поля
+ent_src = tk.Entry(root, width=60)
+ent_dst = tk.Entry(root, width=60)
+expancions_touch = tk.Entry(root, width = 20)
+but = tk.Button(root,  text="Старт", command = touch_adr)
+but.config( width= 11, height = 2)
+ent_dst_title = tk.Label (root, text = 'Введите адрес папки назначения') 
+ent_src_title = tk.Label (root, text = 'Введите путь к исходному каталогу') 
+ent_expansions = tk.Label(root, text = 'Введите расширения файла, например - .txt ')
+
+#Инициализация
+ent_src_title.pack()
+ent_src.pack()
+ent_dst_title.pack()
+ent_dst.pack()
+ent_expansions.pack()
+expancions_touch.pack()
+
+but.place(x=500, y=50)
+
+
+root.mainloop()
+
+
