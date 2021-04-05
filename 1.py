fa7466a76a8cf808c08a95586a501c2a0e18c9a1
Sashamxz <sasha.oom7@gmail.com>
2021-04-05 10:50:54 +0300

    clear

--- 1.py
+++
@@ -1,5 +0,0 @@
-import os
-
-supplies = ['pens', 'staplesrs', 'biders', 'papers' ]
-for i in range (len(supplies)):
-    print('Index ' + str (i) + ' objext is ' + supplies[i] )
--- question/prog.py
+++
@@ -1,8 +0,0 @@
-import requests, bs4
-
-res = requests.get('https://www.i.ua/')
-a = res.raise_for_status()
-print("1---", a)
-
-res_st = bs4.BeautifulSoup(res.txt)
-print('2---', res_st)
