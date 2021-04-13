0d3e82cf422a1afd904bbac5095198f41720369b
Sashamxz <sasha.oom7@gmail.com>
2021-04-13 10:57:03 +0300

    cleaner 0.2.8

--- cleaner_exp.py
+++ cleaner_exp.py
@@ -84,11 +84,11 @@ def cleaner(src_adr, dst_adr, expancion_adr):
     for _file in path_file:
         if _file.endswith(expancion_adr) and os.path.abspath(_file) != dst_adr: # выбираем  фалы по расширению,!=dst
             shutil.move(os.path.abspath(_file), dst_adr)
-            logging.info('file -%s  moved || from --- %s || to--- %s' %(_file,src_adr,dst_adr))
+            logging.info('file -%s    moved || from --- %s || to--- %s' %(_file,src_adr,dst_adr))
             
         elif expancion_adr == '*' and os.path.abspath(_file) != dst_adr:
             shutil.move(os.path.abspath(_file), dst_adr)
-            logging.info('file -%s moved || from --- %s || to--- %s' %(_file,src_adr,dst_adr))
+            logging.info('file -%s    moved || from --- %s || to--- %s' %(_file,src_adr,dst_adr))
             return 4      
         
                                 
